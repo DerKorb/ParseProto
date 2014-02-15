a96718b6802689982609e0b3f241d7d6cb6e49b5
Korbinian Sollner <ksollner@feinarbyte.de>
2014-02-15 16:47:55 +0100

    before split

---
+++ parserThread.js
@@ -0,0 +1,161 @@
+/**
+ * Created by ksollner on 15.02.14.
+ */
+/*
+ * GET home page.
+ */
+async = require("async");
+mongoose = require("mongoose");
+mongoose.connect('mongodb://feinarbyte.de/protoparse');
+
+var Transaction = mongoose.model('Transaction', {
+    address: String,
+    change: Number,
+    block: Number,
+    time: Number,
+    day: Number,
+});
+
+var Donation = mongoose.model('Donation', {
+    address: String,
+    amount: Number,
+    block: Number,
+    time: Number,
+    day: Number,
+});
+
+var bitcoin = require('bitcoin');
+
+var client = new bitcoin.Client({
+    host: 'localhost',
+    port: 3838,
+    user: 'protosharespc',
+    pass: 'alibabatrinktmeinenkaba!'
+});
+
+var currentBlockCount = 0;
+var parsedBlocks = 1;
+var currentBlockTime = 0;
+
+var transactions = {};
+var pts_addresses = {};
+var ags_addresses = {};
+
+function getNextBlock()
+{
+    // get number of blocks
+    var block = {};
+    client.cmd('getblockcount', function (err, block_count)
+    {
+        if (err)
+            return console.log(err);
+
+        // if we already parsed all blocks try again in 10 seconds
+        if (parsedBlocks >= block_count)
+            return setTimeout(getNextBlock, 10000);
+
+
+        block.height = parsedBlocks;
+        currentBlockCount = block_count;
+        if (parsedBlocks%100==0)
+            console.log("block", parsedBlocks);
+
+        // get the block hash
+        client.cmd('getblockhash', parsedBlocks, function(err, block_hash)
+        {
+            if (err)
+                return console.log(err);
+            // get the block
+            block.hash = block_hash;
+            client.cmd('getblock', block_hash , function(err, block_info)
+            {
+                if (err)
+                    return console.log(err);
+
+                // update current block time
+                currentBlockTime = block_info.time;
+                block.time = block_info.time;
+
+                // get all transactions
+                block.rawtransactions = [];
+                block.transactions = [];
+                // Query this block's transaction infos before processing them
+                async.mapSeries(block_info.tx, function(txid, cb)
+                {
+                    client.cmd('getrawtransaction', txid, function(err, rawtransaction)
+                    {
+                        if (err)
+                            console.log(err, txid);
+                        client.cmd('decoderawtransaction', rawtransaction, cb);
+                    });
+                    // process them
+                }, function(err, block_transactions)
+                {
+                    if (err)
+                        console.log(err);
+                    block_transactions.forEach(function(transaction_info)
+                    {
+                        var outputs = [];
+                        var inputs = [];
+                        var angelOutput = 0;
+                        transaction_info.vin.forEach(function(input)
+                        {
+                            if (!input.txid) // ignore mined coins
+                                return;
+                            var input_address = transactions[input.txid].outputs[input.vout].address;
+                            inputs.push({address: input_address});
+
+                            // update protoshares
+                            var pts_address = pts_addresses[input_address];
+                            pts_address.transactions.push({change: -pts_address.balance, time: block_info.time});
+
+                            // update database:
+                            var trans = new Transaction({address: input_address, change: -pts_address.balance, block: parsedBlocks, time: block_info.time, day: Math.ceil(block_info.time/86400)});
+                            trans.save();
+                            pts_address.balance = 0;
+                        });
+                        transaction_info.vout.forEach(function(output)
+                        {
+                            var output_address = output.scriptPubKey.addresses[0];
+                            outputs.push({address: output_address, value: output.value});
+
+                            // update protoshares
+                            if (!pts_addresses[output_address])
+                                pts_addresses[output_address] = {transactions: [], ags_donations: [], balance: 0};
+
+                            pts_addresses[output_address].balance+=output.value;
+                            pts_addresses[output_address].transactions.push({change: output.value, time: block_info.time});
+                            // update database:
+                            var trans = new Transaction({address: output_address, change: output.value, block: parsedBlocks, time: block_info.time, day: Math.ceil(block_info.time/86400-16015)});
+                            trans.save();
+
+                            // update ags
+                            if (output_address == "PaNGELmZgzRQCKeEKM6ifgTqNkC4ceiAWw")
+                            {
+                                var donation_address = inputs[0].address;
+                                var donation = new Donation({address: donation_address, amount: output.value, block: parsedBlocks, time: block_info.time, day: Math.ceil(block_info.time/86400-16015)});
+                                donation.save();
+
+                            }
+                        });
+                        transactions[transaction_info.txid] = {inputs: inputs, outputs: outputs};
+
+                        if (angelOutput>0)
+                            console.log(inputs[0].addresses[0], "=>", angelOutput);
+
+                    });
+                    getNextBlock(++parsedBlocks);
+                });
+            });
+        });
+    });
+}
+
+Transaction.remove({}, function(err, result)
+{
+    Donation.remove({}, function(err, result)
+    {
+        getNextBlock();
+    });
+});
+
--- routes/index.js
+++ routes/index.js
@@ -2,145 +2,76 @@
  * GET home page.
  */
 async = require("async");
-/*mongoose = require("mongoose");
-mongoose.connect('mongodb://protoparse');
+mongoose = require("mongoose");
+mongoose.connect('mongodb://feinarbyte.de/protoparse');
 
-var transaction = mongoose.model({
+var Transaction = mongoose.model('Transaction', {
 	address: String,
 	change: Number,
+    block: Number,
 	time: Number,
-	toAngel: Boolean
-});*/
+    day: Number,
+});
+
+var Donation = mongoose.model('Donation', {
+	address: String,
+	amount: Number,
+    block: Number,
+	time: Number,
+    day: Number,
+});
 
 exports.index = function(req, res){
 	res.render('index', { title: 'Express' });
 };
 
 exports.ptsJson = function(req, res){
-    var timestamp = false;
+
+
     var dateParts = req.query.date.split("-");
-    timestamp = new Date(dateParts[2],dateParts[0],dateParts[1]).getTime()/1000;
+    var timestamp = new Date(dateParts[2],dateParts[0],dateParts[1]).getTime()/1000;
 	result = {blocknum: parsedBlocks, blocktime: currentBlockTime, balances: [], supply: 0};
-	for(key in timestamp ? pts_addresses.filter : pts_addresses)
-	{
-		result.supply+=pts_addresses[key];
-		address = {};
-		address[key] = pts_addresses[key];
-		result.balances.push(address);
-	}
+	for (address in pts_addresses)
+    {
+        var sum = 0;
+        pts_addresses[address].transactions.forEach(function(transaction)
+        {
+            if (transaction.time < timestamp)
+                sum += transaction.change;
+        });
+        paddress = {};
+        paddress[address] = sum;
+        if (sum>0)
+        {
+            result.balances.push(paddress)
+            result.supply += sum;
+        }
+    }
 	res.send(result);
 };
 
 exports.agsJson = function(req, res){
+    var dateParts = req.query.date.split("-");
+    var timestamp = new Date(dateParts[2],dateParts[0],dateParts[1]).getTime()/1000;
+    result = {blocknum: parsedBlocks, blocktime: currentBlockTime, balances: [], supply: 0};
+    for (address in pts_addresses)
+    {
+        var sum = 0;
+        pts_addresses[address].ags_donations.forEach(function(ags_donation)
+        {
+            if (ags_donation.time < timestamp)
+                sum += ags_donation.amount;
+        });
+        paddress = {};
+        paddress[address] = sum;
+        if (sum>0)
+        {
+            result.balances.push(paddress)
+            result.supply += sum;
+        }
+    }
+    res.send(result);
 }
 
 exports.genesisBlock = function(req, res){
 }
-
-var bitcoin = require('bitcoin');
-
-var client = new bitcoin.Client({
-	host: 'localhost',
-	port: 3838,
-	user: 'mytestuser',
-	pass: 'alibabatrinktmeinenkaba!'
-});
-
-var currentBlockCount = 0;
-var parsedBlocks = 0;
-var currentBlockTime = 0;
-
-var transactions = {};
-var pts_addresses = {};
-var ags_addresses = {};
-
-function getNextBlock()
-{
-    // get number of blocks
-	var block = {};
-	client.cmd('getblockcount', function (err, block_count)
-	{
-		if (err)
-			return console.log(err);
-
-        // if we already parsed all blocks try again in 10 seconds
-		if (parsedBlocks >= block_count)
-			return setTimeout(getNextBlock, 10000);
-
-		parsedBlocks++;
-		block.height = parsedBlocks;
-		currentBlockCount = block_count;
-		if (parsedBlocks%100==0)
-			console.log("block", parsedBlocks);
-
-		// get the block hash
-        client.cmd('getblockhash', parsedBlocks, function(err, block_hash)
-        {
-            if (err)
-                return console.log(err);
-            // get the block
-	        block.hash = block_hash;
-            client.cmd('getblock', block_hash , function(err, block_info)
-            {
-                if (err)
-                    return console.log(err);
-
-                // update current block time
-                currentBlockTime = block_info.time;
-				block.time = block_info.time;
-
-                // get all transactions
-	            block.rawtransactions = [];
-	            block.transactions = [];
-	            // Query this block's transaction infos before processing them
-                async.mapSeries(block_info.tx, function(txid, cb)
-	            {
-	                client.cmd('getrawtransaction', txid, function(err, rawtransaction)
-                    {
-                        client.cmd('decoderawtransaction', rawtransaction, cb);
-                    });
-	            // process them
-                }, function(err, transactions)
-                {
-	                transactions.forEach(function(transaction_info)
-	                {
-		                var outputs = [];
-		                var inputs = [];
-		                var angelOutput = 0;
-		                transaction_info.vin.forEach(function(input)
-		                {
-			                if (!input.txid || !transactions[input.txid])
-				                return;
-			                if (!transactions[input.txid].outputs[input.vout])
-				                console.log(transactions[input.txid].outputs, input.vout);
-			                if (transactions[input.txid].outputs[input.vout].addresses.length > 1)
-			                    conole.log("more than one adresses in array");
-			                inputs.push({addresses: transactions[input.txid].outputs[input.vout].addresses});
-			                // update protoshares
-			                delete pts_addresses[transactions[input.txid].outputs[input.vout].addresses];
-		                });
-		                transaction_info.forEach(function(output)
-		                {
-			                outputs.push({addresses: output.scriptPubKey.addresses, value: output.value});
-			                if (output.scriptPubKey.addresses == "PaNGELmZgzRQCKeEKM6ifgTqNkC4ceiAWw")
-			                    angelOutput = output.value;
-			                // update protoshares
-			                if (!pts_addresses[output.scriptPubKey.addresses])
-				                pts_addresses[output.scriptPubKey.addresses] = {transactions: [], agsTransactions: [], balance: 0};
-			                pts_addresses[output.scriptPubKey.addresses].balance+=output.value;
-			                pts_addresses[output.scriptPubKey.addresses].transactions+=output.value;
-		                });
-		                transactions[transaction_info.txid] = {inputs: inputs, outputs: outputs};
-
-		                if (angelOutput>0)
-		                    console.log(inputs[0].addresses[0], "=>", angelOutput);
-
-	                });
-	                getNextBlock(++parsedBlocks);
-                });
-            });
-        });
-	});
-}
-
-getNextBlock();
