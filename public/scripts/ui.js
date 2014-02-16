f2416c36f21a93b5990da0042473f17882a76596
Korbinian Sollner <ksollner@feinarbyte.de>
2014-02-16 19:07:52 +0100

    disable button on click and inform about waiting time

--- public/scripts/ui.js
+++ public/scripts/ui.js
@@ -8,6 +8,7 @@ $(function () {
     $("#sliderags").slider({min: 0, max: 100, value: 10});
     $("#sliderpts").slider({min: 0, max: 100, value: 10});
     $("#go").button().click(function () {
+        $("#go").attr("disabled","disabled").button("option", "label", "loading - please be patient");
         location.href = "/" + $('input[name=action]:checked').val() + "?date=" + $("#datepicker").val().replace(/\//g, "-") + ($('input[name=action]:checked').val() == "genesis" ? "&supply=" + $("#supply").val() + "&portionPts=" + $("#sliderpts").slider('value')/100 + "&portionAgs=" + $("#sliderags").slider('value')/100 : "");
     });
 });
