7770681f06a3ae8d87d2a050d5466f64ddca545b
Korbinian Sollner <ksollner@feinarbyte.de>
2014-02-16 03:37:37 +0100

    mysql addresses as int

--- routes/index.js
+++ routes/index.js
@@ -18,8 +18,7 @@ exports.index = function(req, res){
 
 exports.ptsJson = function(req, res){
     var dateParts = req.query.date.split("-");
-    var day = Math.floor(new Date(dateParts[2],dateParts[0]-1,dateParts[1]).getTime()/1000/86400-16014);
-    console.log("queried");
+    var day = Math.floor(new Date(dateParts[2],dateParts[0]-1,dateParts[1]).getTime()/1000/86400-16014)+1;
     connection.query("SELECT SUM(`change`)/100000000 supply, MAX(time) time, MAX(block) blockheight FROM transactions2 WHERE day <= ?;" +
         "SELECT address, sum(`change`)/100000000 balance FROM transactions2 JOIN addresses ON id_address = id WHERE day <= ?  GROUP BY id_address HAVING balance>0", [day, day], function(err, block)
     {
@@ -29,35 +28,32 @@ exports.ptsJson = function(req, res){
         result.balances = [];
         block[1].forEach(function(balance)
         {
-           var addr = {};
-           addr[balance.address] = balance.balance;
-           result.balances.push(addr);
+           result.balances.push([balance.address,balance.balance]);
         });
-        console.log("send");
         res.send(result);
     });
 };
 
 exports.agsJson = function(req, res){
     var dateParts = req.query.date.split("-");
-    var day = Math.floor(new Date(dateParts[2],dateParts[0]-1,dateParts[1]).getTime()/1000/86400-16014);
+    console.log(dateParts);
+    console.log(new Date(dateParts[2],dateParts[0]-1,dateParts[1]));
+    console.log(new Date(dateParts[2],dateParts[0]-1,dateParts[1]).getTime()/1000);
+    var day = Math.floor(new Date(dateParts[2],dateParts[0]-1,dateParts[1]).getTime()/1000/86400-16014)+1;
     connection.query(
-        "CREATE TEMPORARY TABLE IF NOT EXISTS ags_rates AS (SELECT SUM(`amount`/100000000) ags_rate, IF(day<57, 57, day) as day2 FROM donations GROUP BY IF(day<57, 57, day)); " +
-        "SELECT addresses.address, MAX(block) blockheight, MAX(time) time, SUM(`amount`)/ags_rate/20000 balance FROM donations JOIN ags_rates ON IF(day<57, 57, day) = day2 JOIN addresses ON donations.address = addresses.id WHERE day <= ? GROUP BY addresses.id ORDER BY addresses.address;" +
-            "DROP TEMPORARY TABLE ags_rates", [day], function(err, block)
+        "DROP TEMPORARY TABLE IF EXISTS ags_rates;" +
+    "CREATE TEMPORARY TABLE IF NOT EXISTS ags_rates AS (SELECT CAST(5000/SUM(`amount`) AS DECIMAL(39,30)) ags_rate, IF(day<57, 57, day) as day2 FROM donations GROUP BY IF(day<57, 57, day));" +
+    "SELECT addresses.address, MAX(block) blockheight, MAX(time) time, ROUND(SUM(`amount`*ags_rate), 8) balance FROM donations JOIN ags_rates ON IF(day<57, 57, day) = day2 JOIN addresses ON donations.address = addresses.id WHERE day <= ? GROUP BY addresses.id ORDER BY addresses.address;", [day], function(err, block)
     {
-        console.log(err, block);
         var result = {blockheight: 0, time: 0, supply: 0, balances: []};
-        block[1].forEach(function(balance)
+        block[2].forEach(function(balance)
         {
-            var addr = {};
-            addr[balance.address] = balance.balance;
             if (balance.blockheight > result.blockheight)
                 result.blockheight = balance.blockheight;
             if (balance.time > result.time)
                 result.time = balance.time;
             result.supply += balance.balance;
-            result.balances.push(addr);
+            result.balances.push([balance.address,balance.balance]);
         });
         res.send(result);
     });
