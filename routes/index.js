b54e0f20bb63698a84eeb765daa1bdcfe9308579
DerKorb <hackin@gmx.de>
2014-02-17 03:06:34 +0100

    small changes for btc_ags

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@
 async = require("async");
 mysql = require("mysql");
 connection = mysql.createConnection({
-	host: process.env.dbhost ? process.env.dbhost : "localhost",
-	user: "parse_user",
+    host: process.env.dbhost ? process.env.dbhost : "localhost",
+    user: "parse_user",
     password: "mQhURtm4qaLbsxl",
     database: "parse_db",
     multipleStatements: true
@@ -32,30 +32,27 @@ exports.ptsJson = function (req, res) {
     });
 };
 
+queryAgs = function(day, cb)
+{
+	connection.query(
+		"DROP TEMPORARY TABLE IF EXISTS ags_rates;" +
+			"CREATE TEMPORARY TABLE IF NOT EXISTS ags_rates AS (SELECT CAST(5000/SUM(`amount`) AS DECIMAL(39,30)) ags_rate, IF(day<57, 57, day) as day2 FROM donations GROUP BY IF(day<57, 57, day));" +
+			"SELECT addresses.address, MAX(block) blockheight, MAX(time) time, ROUND(SUM(`amount`*ags_rate), 8) balance FROM donations JOIN ags_rates ON IF(day<57, 57, day) = day2 JOIN addresses ON donations.address = addresses.id WHERE day <= ? GROUP BY addresses.id ORDER BY addresses.address;" +
+			"DROP TEMPORARY TABLE IF EXISTS ags_rates_btc;" +
+			"CREATE TEMPORARY TABLE IF NOT EXISTS ags_rates_btc AS (SELECT CAST(5000/SUM(`amount`) AS DECIMAL(39,30)) ags_rate, IF(day<57, 57, day) as day2 FROM donations_btc GROUP BY IF(day<57, 57, day));" +
+			"SELECT addresses_btc.address, MAX(block) blockheight, MAX(time) time, ROUND(SUM(`amount`*ags_rate), 8) balance FROM donations_btc JOIN ags_rates_btc ON IF(day<57, 57, day) = day2 JOIN addresses_btc ON donations_btc.address = addresses_btc.id WHERE day <= ? GROUP BY addresses_btc.id ORDER BY addresses_btc.address;", [day, day],cb);
+}
+
 exports.agsJson = function (req, res) {
     var dateParts = req.query.date.split("-");
     var day = Math.floor(new Date(dateParts[2], dateParts[0] - 1, dateParts[1]).getTime() / 1000 / 86400 - 16014) + 1;
-    connection.query(
-        "DROP TEMPORARY TABLE IF EXISTS ags_rates;" +
-            "CREATE TEMPORARY TABLE IF NOT EXISTS ags_rates AS (SELECT CAST(5000/SUM(`amount`) AS DECIMAL(39,30)) ags_rate, IF(day<57, 57, day) as day2 FROM donations GROUP BY IF(day<57, 57, day));" +
-            "SELECT addresses.address, MAX(block) blockheight, MAX(time) time, ROUND(SUM(`amount`*ags_rate), 8) balance FROM donations JOIN ags_rates ON IF(day<57, 57, day) = day2 JOIN addresses ON donations.address = addresses.id WHERE day <= ? GROUP BY addresses.id ORDER BY addresses.address;" +
-	        "DROP TEMPORARY TABLE IF EXISTS ags_rates_btc;" +
-            "CREATE TEMPORARY TABLE IF NOT EXISTS ags_rates_btc AS (SELECT CAST(5000/SUM(`amount`) AS DECIMAL(39,30)) ags_rate, IF(day<57, 57, day) as day2 FROM donations_btc GROUP BY IF(day<57, 57, day));" +
-            "SELECT addresses_btc.address, MAX(block) blockheight, MAX(time) time, ROUND(SUM(`amount`*ags_rate), 8) balance FROM donations_btc JOIN ags_rates_btc ON IF(day<57, 57, day) = day2 JOIN addresses_btc ON donations_btc.address = addresses_btc.id WHERE day <= ? GROUP BY addresses_btc.id ORDER BY addresses_btc.address;", [day, day], function (err, block) {
-            var result = {blockheight: 0, time: 0, supply: 0, balances: []};
+	queryAgs(day, function (err, block) {
+            var result = {supply: 0, balances: []};
             block[2].forEach(function (balance) {
-                if (balance.blockheight > result.blockheight)
-                    result.blockheight = balance.blockheight;
-                if (balance.time > result.time)
-                    result.time = balance.time;
                 result.supply += balance.balance;
                 result.balances.push([balance.address, balance.balance]);
             });
             block[5].forEach(function (balance) {
-                if (balance.blockheight > result.blockheight)
-                    result.blockheight = balance.blockheight;
-                if (balance.time > result.time)
-                    result.time = balance.time;
                 result.supply += balance.balance;
                 result.balances.push([balance.address, balance.balance]);
             });
@@ -66,13 +63,9 @@ exports.agsJson = function (req, res) {
 exports.genesisBlock = function (req, res) {
     var dateParts = req.query.date.split("-");
     var day = Math.floor(new Date(dateParts[2], dateParts[0] - 1, dateParts[1]).getTime() / 1000 / 86400 - 16014) + 1;
-    connection.query(
-	    "DROP TEMPORARY TABLE IF EXISTS ags_rates;" +
-		    "CREATE TEMPORARY TABLE IF NOT EXISTS ags_rates AS (SELECT CAST(5000/SUM(`amount`) AS DECIMAL(39,30)) ags_rate, IF(day<57, 57, day) as day2 FROM donations GROUP BY IF(day<57, 57, day));" +
-		    "SELECT addresses.address, MAX(block) blockheight, MAX(time) time, ROUND(SUM(`amount`*ags_rate), 8) balance FROM donations JOIN ags_rates ON IF(day<57, 57, day) = day2 JOIN addresses ON donations.address = addresses.id WHERE day <= ? GROUP BY addresses.id ORDER BY addresses.address;" +
-		    "DROP TEMPORARY TABLE IF EXISTS ags_rates_btc;" +
-		    "CREATE TEMPORARY TABLE IF NOT EXISTS ags_rates_btc AS (SELECT CAST(5000/SUM(`amount`) AS DECIMAL(39,30)) ags_rate, IF(day<57, 57, day) as day2 FROM donations_btc GROUP BY IF(day<57, 57, day));" +
-		    "SELECT addresses_btc.address, MAX(block) blockheight, MAX(time) time, ROUND(SUM(`amount`*ags_rate), 8) balance FROM donations_btc JOIN ags_rates_btc ON IF(day<57, 57, day) = day2 JOIN addresses_btc ON donations_btc.address = addresses_btc.id WHERE day <= ? GROUP BY addresses_btc.id ORDER BY addresses_btc.address;", [day, day, day], function (err, pts_result) {
+    queryAgs(day, function (err, ags_result) {
+            connection.query("SELECT SUM(`change`)/100000000 supply, MAX(time) time, MAX(block) blockheight FROM transactions2 WHERE day <= ?;" +
+                "SELECT address, sum(`change`)/100000000 balance FROM transactions2 JOIN addresses ON id_address = id WHERE day <= ?  GROUP BY id_address HAVING balance>0", [day, day], function (err, pts_result) {
                 if (err)
                     throw(err);
                 var result = {};
